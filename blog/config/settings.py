624a2fea38167e1d7216a373c7c21146bee53340
K^+pj8 <mahmoudreda457@gmail.com>
2023-02-20 10:12:48 +0200

    add forms & user accounts

--- blog/config/settings.py
+++ blog/config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',  # new
+    'blog',
+    'accounts',   # new
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,7 @@ STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]  # new
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Regestration                                         # new
+LOGIN_REDIRECT_URL = "home"  
+LOGOUT_REDIRECT_URL = "home" 
