11126db265b1cb6b56d133184dbe0747ff56bf34
jrreda <mahmoudreda457@gmail.com>
2023-03-11 12:00:01 +0200

    add permessions

--- bookstore/books/views.py
+++ bookstore/books/views.py
@@ -1,16 +1,21 @@
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    PermissionRequiredMixin,
+)  # new
 from django.views.generic import ListView, DeleteView
 
 from .models import Book
 
 
 # Create your views here.
-class BookListView(ListView):
+class BookListView(LoginRequiredMixin, ListView):  # new
     model = Book
     template_name = "books/book_list.html"
     context_object_name = "book_list"
 
 
-class BookDetailView(DeleteView):
+class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):  # new
     model = Book
     template_name = "books/book_detail.html"
     context_object_name = "book"
+    permission_required = "books.special_status"
