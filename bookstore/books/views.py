f5f7d92aeda2f48ae90dfe3164ee2269f3924d54
jrreda <mahmoudreda457@gmail.com>
2023-03-11 14:21:53 +0200

    add search

--- bookstore/books/views.py
+++ bookstore/books/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     PermissionRequiredMixin,
 )  # new
+from django.db.models import Q  # new
 from django.views.generic import ListView, DeleteView
 
 from .models import Book
@@ -19,3 +20,15 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):  
     template_name = "books/book_detail.html"
     context_object_name = "book"
     permission_required = "books.special_status"
+
+
+class SearchResultsListView(ListView):  # new
+    model = Book
+    context_object_name = "book_list"
+    template_name = "books/search_results.html"
+
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(title__icontains=query)
+        )
