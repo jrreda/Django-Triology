da4747ba10e4d783576400829abe8bbf4864ee15
jrreda <mahmoudreda457@gmail.com>
2023-03-14 17:44:00 +0200

    add 1st DRF api

--- library/django_project/settings.py
+++ library/django_project/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third Party
+    "rest_framework",  # new
     # Local
     "books",
+    "apis",  # new
 ]
 
 MIDDLEWARE = [
